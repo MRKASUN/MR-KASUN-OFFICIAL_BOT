0acfa5c645ba7539b4468c143b074f0af778330c
MR_KASUN OFFICIAL <160117382+MRKASUN@users.noreply.github.com>
2024-03-05 16:38:38 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+	"name": "DARK FEENIX",
+	"description": "WhatsApp Bot Using Lib Baileys",
+	"repository": "https://github.com/MRKASUN/MR-KASUN-OFFICIAL_BOT",
+	"logo": "https://node-js-sample.herokuapp.com/node.png",
+	"keywords": ["multi-device"]
+}
